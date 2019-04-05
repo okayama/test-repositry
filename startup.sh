4599678ad811634ee4c9cfdcf5fc61ad480b1308
okayama <kondoh@alfasado.jp>
2019-04-05 12:42:31 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#####
+##### ACTIVE
 echo 'test' > startup_echo.txt
 pwd > startup_pwd.txt
+# pwd is /home/site/wwwroot
 #####
 
 #####
-echo 'test' > /home/sites/wwwroot/startup_echo2.txt
-pwd > /home/sites/wwwroot/startup_pwd2.txt
+echo 'test' > /home/site/wwwroot/startup_echo2.txt
+pwd > /home/site/wwwroot/startup_pwd2.txt
 #####

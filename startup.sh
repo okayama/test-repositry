b96a7d5b4b6599053038f9b6964064e822871c24
okayama <kondoh@alfasado.jp>
2019-04-05 12:30:34 +0900

    Add startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#####
+echo 'test' > startup_echo.txt
+pwd > startup_pwd.txt
+#####
+
+#####
+echo 'test' > /home/sites/wwwroot/startup_echo2.txt
+pwd > /home/sites/wwwroot/startup_pwd2.txt
+#####
